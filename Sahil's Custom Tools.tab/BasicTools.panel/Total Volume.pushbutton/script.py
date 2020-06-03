41505e7be1f4352dc5790e68a4fa307238fa58c6
Joe Biden <36573858+stadwalkar@users.noreply.github.com>
2020-06-03 15:09:10 -0700

    first commit

---
+++ Sahil's Custom Tools.tab/BasicTools.panel/Total Volume.pushbutton/script.py
@@ -0,0 +1,29 @@
+#importing relevant namespaces
+
+import clr
+from Autodesk.Revit.DB import *
+from Autodesk.Revit.DB.Architecture import *
+from Autodesk.Revit.DB.Analysis import *
+from Autodesk.Revit.UI import *
+from Autodesk.Revit import DB
+from Autodesk.Revit import UI
+doc = __revit__.ActiveUIDocument.Document
+
+__title__ = 'Total Volume'
+__author__ = 'Sahil'
+
+#collector instance
+cl = DB.FilteredElementCollector(doc)
+cl.OfCategory(BuiltInCategory.OST_Walls)
+cl.WhereElementIsNotElementType()
+
+#iterate and volume 
+total_volume = 0.0
+
+for wall in cl:
+	vol_param = wall.LookupParameter('Volume')
+	if vol_param:
+		total_volume = total_volume + vol_param.AsDouble()
+
+#results
+print("Total Volume is: {}".format(total_volume))
